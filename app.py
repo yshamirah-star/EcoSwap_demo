84ac67b343e6b7686cab0c44518b82f49984a6df
yshamirah-star <yshamirah@gmail.com>
2026-02-24 16:17:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -37,11 +37,11 @@ selected_waste = st.selectbox("Choose Waste Type", waste_types)
 if st.button("Generate Waste Image"):
     # Placeholder: random image URLs (replace with your own or integrate later)
     sample_images = {
-       "Plastics": "https://images.pexels.com/photos/10368560/pexels-photo-10368560.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-        "Paper": "https://images.pexels.com/photos/7161725/pexels-photo-7161725.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-        "Textiles": "https://images.pexels.com/photos/4099516/pexels-photo-4099516.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-        "Organics": "https://images.pexels.com/photos/6157032/pexels-photo-6157032.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-        "Mixed_Industrial": "https://images.pexels.com/photos/3316790/pexels-photo-3316790.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+       "Plastics": "https://images.unsplash.com/photo-1579547621617-be0876d7294b?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", # Pexels plastic waste
+        "Paper": "https://images.unsplash.com/photo-1629815077478-f7564cc49195?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",   # Pexels paper waste
+        "Textiles": "https://images.unsplash.com/photo-1549429462-df21f42d2a45?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", # Pexels textile waste
+        "Organics": "https://images.unsplash.com/photo-1582234057861-c852e008b89e?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", # Pexels organic waste
+        "Mixed_Industrial": "https://images.unsplash.com/photo-1620892976077-d652f1e626e2?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" # Pexels industrial waste
     }
     st.image(sample_images[selected_waste], caption=f"{selected_waste} Waste", use_column_width=True)
     
